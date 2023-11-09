80a6cedea3468fc5fd097f13d12a9ab214c21a1c
Matt Bauman <mbauman@juliacomputing.com>
2023-11-09 09:44:57 -0500

    better rand! benchmarks and results

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 Simple fork of the Random stdlib that changes the definition of `rand()` for floating point numbers such that:
 
-* For `Float16` it returns values in [0, 1) as though a random variable were drawn from a theoretically ideal uniform distribution and then rounded down to the previous floating point number.
-* For `Float32`, and `Float64` it return values in [0, 1) as though a random variable were drawn from a theoretically ideal uniform distribution and then rounded down the previous multiple of 2^-64 (or to the previous floating point number).
+* For `Float16`, `Float32`, and `Float64` it returns values in [0, 1) as though a random variable were drawn from a theoretically ideal uniform distribution and then rounded down the previous multiple of 2^-64 (or to the previous floating point number).
 
 The current floating implementation currently rounds down to the previous multiple of `eps(T)/2` for all three types.
 
-Performance is slightly worse (particularly for Float32), but less than 2x on an Apple M1:
+Concretely, this prototype means that every `Float16` value `x ∈ [0,1)` is a possible output, chosen with probability `eps(x)`. For `Float32`, the possible outputs are multiples of 2^-64 in `[0, 2^-49)`, chosen with probability 2^-64, and every `x ∈ [2^-49, 1)`, chosen with probability `eps(x)`.  And `Float64` returns multiples of 2^-64 up to a value of 2^-11.  As a table:
+
+  | Implementation | Possible Outputs | P(rand() = x) |
+  | ---- | ---- | ---- |
+  | Random.rand(::T) | multiples of `eps(T(0.5))` in `[0, 1)`     |  `eps(T(0.5))`    |
+  | SR.rand(::Float16)     | all `x ∈ [0,1)`     | `eps(x)`    |
+  | SR.rand(::Float32)     | multiples of 2^-64 in `[0, 2^-49)` <br> all `x ∈ [2^-49,1)`     | `2^-64` <br> `eps(x)`    |
+  | SR.rand(::Float64)     | multiples of 2^-64 in `[0, 2^-11)` <br> all `x ∈ [2^-11,1)`     | `2^-64` <br> `eps(x)`    |
+
+Performance is about the same for scalars. For the SIMD `rand!`, performance is slightly worse on a M1, but no worse than 2x (most affected is `rand!(::Vector{Float32})`):
 
 ![violin graphs comparing performance](scalar.svg "Scalar Performance")
 
-![grid of violin graphs](array.svg "Array filling performance with rand!")
+![line graph of rand!</code>'s time per element for varying array sizes](array.svg "Array filling performance with rand!")
+
+And on an Intel system with AVX-512 scalar performance is similar but array performance is nearly equivalent:
+
+![above line graph as executed on system with AVX-512](array-avx.svg "Array filling performance with rand! with AVX-512")
--- bench.jl
+++ bench.jl
@@ -1,5 +1,6 @@
 import Random, ShawshankRandemption
-using StatsPlots: StatsPlots, plot, savefig, @layout, ylims, ylims!
+using StatsPlots: StatsPlots, plot, plot!, savefig, @layout, ylims, ylims!
+using Statistics: median, quantile
 using BenchmarkTools: BenchmarkTools, BenchmarkGroup, @benchmark, @benchmarkable
 import BenchmarkPlots
 
@@ -16,14 +17,14 @@ scalars["Float64"]["Random"] = @benchmarkable Random.rand(Float64)
 scalars["Float64"]["ShawshankRandemption"] = @benchmarkable ShawshankRandemption.rand(Float64)
 
 BenchmarkTools.tune!(scalars)
-t = BenchmarkTools.run(scalars)
-p = plot([plot(t["$T"], title="rand($T)") for T in (Float16, Float32, Float64)]..., layout=@layout([a b c]), titlefontsize=8, tickfontsize=4, labelfontsize=4)
-ylims!.(p.subplots, tuple.(0, last.(ylims.(p.subplots))))
+ts = BenchmarkTools.run(scalars)
+p = plot([plot(ts["$T"], title="rand($T)") for T in (Float16, Float32, Float64)]..., layout=@layout([a b c]), titlefontsize=8, tickfontsize=4, labelfontsize=4)
+ylims!.(p.subplots, tuple.(0, 5 #=last.(ylims.(p.subplots)))=#))
 savefig(p, "scalar.svg")
 
 ## Arrays
 arrays = BenchmarkGroup()
-sizes = round.(Int, exp2.(2:7:24))
+sizes = round.(Int, exp2.(2:1.75:25))
 arrays["Float16"] = BenchmarkGroup()
 for n in sizes
     arrays["Float16"]["$n"] = BenchmarkGroup()
@@ -44,8 +45,19 @@ for n in sizes
 end
 
 BenchmarkTools.tune!(arrays)
-t = BenchmarkTools.run(arrays)
+ta = BenchmarkTools.run(arrays)
 
-p = plot([plot(t["$T"]["$n"], title="$n x $T") for n in sizes, T in (Float16, Float32, Float64)]..., titlefontsize=8, tickfontsize=4, labelfontsize=4)
-ylims!.(p.subplots, tuple.(0, last.(ylims.(p.subplots))))
+# p = plot([plot(t["$T"]["$n"], title="$n x $T") for n in sizes, T in (Float16, Float32, Float64)]..., titlefontsize=8, tickfontsize=4, labelfontsize=4)
+# ylims!.(p.subplots, tuple.(0, last.(ylims.(p.subplots))))
+
+p = plot(xaxis=:log, xlabel="Array length", ylabel="Average ns/elt (min - median - 90%)")
+colors = StatsPlots.palette(:Paired_6)
+for (i,T) in enumerate((Float16, Float32, Float64))
+    r =  [f(ta["$T"]["$n"]["Random"]).time/n for n in sizes, f in (minimum, median, x->(;time=quantile(x.times, .9)))]
+    plot!(p, sizes, r[:,2], ribbon=(r[:,2].-r[:,1], r[:,3].-r[:,2]), label="Random $T", color=colors[i*2])
+    s =  [f(ta["$T"]["$n"]["ShawshankRandemption"]).time/n for n in sizes, f in (minimum, median, x->(;time=quantile(x.times, .9)))]
+    plot!(p, sizes, s[:,2], ribbon=(s[:,2].-s[:,1], s[:,3].-s[:,2]), label="ShawshankRandemption $T", color=colors[i*2-1], linestyle=:dash)
+end
+ylims!(p, (0, last(ylims(p))))
+
 savefig(p, "array.svg")
